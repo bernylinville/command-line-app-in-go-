ea46020b8ec26e773560577de7d5448b7a74b674
Berny Linville <bernylinville@devopsthink.org>
2022-03-31 10:26:29 +0800

    add delFiles

--- fileSystem/walk/main.go
+++ fileSystem/walk/main.go
@@ -17,6 +17,9 @@ type config struct {
 
 	// list files
 	list bool
+
+	// delete files
+	del bool
 }
 
 func main() {
@@ -24,6 +27,7 @@ func main() {
 	root := flag.String("root", ".", "Root directory to start")
 	// Action options
 	list := flag.Bool("list", false, "List files only")
+	del := flag.Bool("del", false, "Delete files")
 	// Filter options
 	ext := flag.String("ext", "", "File extension to filter out")
 	size := flag.Int64("size", 0, "Minimum file size")
@@ -33,6 +37,7 @@ func main() {
 		ext:  *ext,
 		size: *size,
 		list: *list,
+		del:  *del,
 	}
 
 	if err := run(*root, os.Stdout, c); err != nil {
@@ -56,6 +61,11 @@ func run(root string, out io.Writer, cfg config) error {
 			// If list was explicitly set, don't do anything else
 			if cfg.list {
 				return listFile(path, out)
+			}
+
+			// delete files
+			if cfg.del {
+				return delFile(path)
 			}
 
 			// List is the default option if nothing else was set
